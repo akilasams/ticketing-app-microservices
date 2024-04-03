eb3e25486ec2eaa5be7d1a01c6b9e1674e4b19b4
Akila Samarawickrama <akilaishere@gmail.com>
2024-04-03 07:25:38 +0530

    Sign up and sign up response formatting done

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -22,16 +22,28 @@ interface UserDoc extends mongoose.Document {
   //   updatedAt: string
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password; // delete removes prop from an obj : js
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // Pre-Save Hook
 userSchema.pre("save", async function (done) {
